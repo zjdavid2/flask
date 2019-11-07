8eb49cc64e446651b17faaba97baa7cc07daea68
zjdavid2 <a@ddavid.net>
2019-11-07 18:58:36 -0500

    Why is this in source control

--- config.py
+++
@@ -1,8 +0,0 @@
-class Config(object):
-    """Base config, uses staging database server."""
-    DEBUG = False
-    TESTING = False
-    DB_SERVER = 'mongodb://localhost:27017'
-    SECRET_KEY = "ZQMjAJ3zFroBQfjvJNHE"
-    API_KEY = 'taXbcZo1YsLGy4cUAt5WxHkptFd1M139toaMzqOYe9zul6dBfqBSTZZ643io'
-    UPLOAD_FOLDER = '/img'
